de245bdc6727e24f6fe6c68c4ad1a4c23b982368
Brian Tarricone <brian@tarricone.org>
2007-09-06 09:24:32 +0000

    add xfconf_g_value_free() utility function
    (Old svn revision: 26725)

---
+++ common/xfconf-util.c
@@ -0,0 +1,33 @@
+/*
+ *  xfconf
+ *
+ *  Copyright (c) 2007 Brian Tarricone <bjt23@cornell.edu>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License ONLY.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "xfconf-util.h"
+
+void
+xfconf_g_value_free(GValue *value)
+{
+    if(!value)
+        return;
+    g_value_unset(value);
+    g_free(value);
+}
