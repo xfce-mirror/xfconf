a2d91c0e7ec3fa327b68dbc26809d40a5592307f
Guido Berhoerster <gber@opensuse.org>
2011-03-25 17:52:50 +0100

    Make the abicheck work on Solaris and PPC64 (bug #7416).

--- xfconf/abicheck.sh
+++ xfconf/abicheck.sh
@@ -2,6 +2,7 @@
 #
 # Copyright (c) 2004 The GLib Development Team.
 # Copyright (c) 2005 Benedikt Meurer <benny@xfce.org>.
+# Copyright (c) 2011 Guido Berhoerster <gber@opensuse.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 
-cpp -P -DINCLUDE_INTERNAL_SYMBOLS -DINCLUDE_VARIABLES -DALL_FILES ${srcdir:-.}/xfconf.symbols | sed -e '/^$/d' -e 's/ XFCONF_GNUC.*$//' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE//' | sort > expected-abi
-nm -D .libs/libxfconf-0.so | grep " T\|R " | cut -d ' ' -f 3 | grep -v '^_.*' | sort > actual-abi
-diff -u expected-abi actual-abi && rm expected-abi actual-abi
+trap 'rm expected-abi actual-abi' EXIT
+${CPP:-cpp} -DINCLUDE_INTERNAL_SYMBOLS -DINCLUDE_VARIABLES -DALL_FILES ${srcdir:-.}/xfconf.symbols | sed 's/ G_GNUC.*$//;s/ PRIVATE//;/^ *$/d;/^#/d' | sort >expected-abi
+${NM:-nm} -D -g -P .libs/libxfconf-0.so | awk '$2~/^[DRTG]$/&&$1~/^[^_]/{print $1}' | sort >actual-abi
+diff -u expected-abi actual-abi
