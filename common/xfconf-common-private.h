6561c9fe940d4acb07c5eae63867c46683a02f2c
Brian J. Tarricone <brian@tarricone.org>
2023-10-11 21:41:02 -0700

    Cache property-not-found responses in XfconfCache
    Some apps (like xfce4-terminal) will query certain properties very
    often, and those properties are often not changed from their defaults,
    and thus do not have any value stored in xfconf.  If properties are not
    found, we can cache that state in XfconfCache and avoid xfconfd
    round-trips.  If the property is later set, xfconfd will emit
    PropertyChanged, and we will update our cache with the newly-set value.

--- common/xfconf-common-private.h
+++ common/xfconf-common-private.h
@@ -21,4 +21,11 @@
 #ifndef __XFCONF_COMMON_PRIVATE_H__
 #define __XFCONF_COMMON_PRIVATE_H__
 
+#include <glib.h>
+
+enum _XfconfError;
+
+gboolean _xfconf_error_from_dbus_error_name(const gchar *error_name,
+                                            enum _XfconfError *xfconf_error);
+
 #endif  /* __XFCONF_COMMON_PRIVATE_H__ */
