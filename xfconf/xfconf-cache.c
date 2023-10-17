xfce-mirror/xfconf

commit 6561c9fe940d4acb07c5eae63867c46683a02f2c

Cache property-not-found responses in XfconfCache
Some apps (like xfce4-terminal) will query certain properties very
often, and those properties are often not changed from their defaults,
and thus do not have any value stored in xfconf.  If properties are not
found, we can cache that state in XfconfCache and avoid xfconfd
round-trips.  If the property is later set, xfconfd will emit
PropertyChanged, and we will update our cache with the newly-set value.

xfconf/xfconf-cache.c
@@ -75,14 +75,12 @@ xfconf_cache_item_new(const GValue *value,
 {
     XfconfCacheItem *item;
 
-    g_return_val_if_fail(value, NULL);
-
     item = g_slice_new0(XfconfCacheItem);
 #if 0
     item->last_used = g_get_monotonic_time ();
 #endif
 
-    if(G_LIKELY(steal)) {
+    if(G_LIKELY(steal) || value == NULL) {
         item->value = (GValue *) value;
     } else {
 
@@ -112,7 +110,12 @@ xfconf_cache_item_update(XfconfCacheItem *item,
 #endif
 
     if(value) {
-        g_value_unset(item->value);
+        if (item->value == NULL) {
+            item->value = g_new0(GValue, 1);
+        } else {
+            g_value_unset(item->value);
+        }
+
         g_value_init(item->value, G_VALUE_TYPE(value));
 
         /* We need to dup the array */
@@ -133,8 +136,10 @@ xfconf_cache_item_free(XfconfCacheItem *item)
 {
     g_return_if_fail(item);
 
-    g_value_unset(item->value);
-    g_free(item->value);
+    if (item->value != NULL) {
+        g_value_unset(item->value);
+        g_free(item->value);
+    }
     g_slice_free(XfconfCacheItem, item);
 }
 
@@ -540,12 +545,19 @@ xfconf_cache_handle_property_removed (XfconfCache *cache, GVariant *parameters)
     const gchar *channel_name, *property;
     GValue value = G_VALUE_INIT;
     if (g_variant_is_of_type(parameters, G_VARIANT_TYPE ("(ss)"))) {
+        XfconfCacheItem *item;
+
         g_variant_get(parameters, "(&s&s)", &channel_name, &property);
 
         if(strcmp(channel_name, cache->channel_name))
             return;
 
-        g_tree_remove(cache->properties, property);
+        item = g_tree_lookup(cache->properties, property);
+        if (item != NULL && item->value != NULL) {
+            g_value_unset(item->value);
+            g_free(item->value);
+            item->value = NULL;
+        }
 
         g_signal_emit(G_OBJECT(cache), signals[SIG_PROPERTY_CHANGED], 0,
                       cache->channel_name, property, &value);
@@ -643,7 +655,7 @@ xfconf_cache_set_property_reply_handler(GDBusProxy *proxy,
         g_signal_emit(G_OBJECT(cache), signals[SIG_PROPERTY_CHANGED],
                       g_quark_from_string(old_item->property),
                       cache->channel_name, old_item->property,
-                      item ? item->value : &empty_val);
+                      item && item->value ? item->value : &empty_val);
         xfconf_cache_mutex_lock(cache);
     }
 
@@ -763,12 +775,35 @@ xfconf_cache_lookup_locked(XfconfCache *cache,
             g_tree_insert(cache->properties, g_strdup(property), item);
             g_variant_unref (variant);
             /* TODO: check tree for evictions */
-        } else
+        } else {
+            if (g_dbus_error_is_remote_error(tmp_error)) {
+                gchar *error_name = g_dbus_error_get_remote_error(tmp_error);
+                XfconfError xfconf_error;
+
+                if (_xfconf_error_from_dbus_error_name(error_name, &xfconf_error) &&
+                    xfconf_error == XFCONF_ERROR_PROPERTY_NOT_FOUND)
+                {
+                    // Cache a property-not-found response
+                    item = xfconf_cache_item_new(NULL, TRUE);
+                    g_tree_insert(cache->properties, g_strdup(property), item);
+                }
+
+                g_free(error_name);
+            }
+
             g_propagate_error(error, tmp_error);
+        }
     }
 
     if(item) {
-        if(value) {
+        if (item->value == NULL) {
+            g_set_error(error,
+                        XFCONF_ERROR,
+                        XFCONF_ERROR_PROPERTY_NOT_FOUND,
+                        _("Property \"%s\" does not exist on channel \"%s\""),
+                        property,
+                        cache->channel_name);
+        } else if(value) {
             if(!G_VALUE_TYPE(value))
                 g_value_init(value, G_VALUE_TYPE(item->value));
 
@@ -798,7 +833,7 @@ xfconf_cache_lookup_locked(XfconfCache *cache,
 #endif
     }
 
-    return !!item;
+    return item != NULL && item->value != NULL;
 }
 
 gboolean
@@ -889,7 +924,7 @@ xfconf_cache_set(XfconfCache *cache,
         }
     } else {
         old_item = xfconf_cache_old_item_new(cache, property);
-        if(item)
+        if(item && item->value)
             old_item->item = xfconf_cache_item_new(item->value, FALSE);
         g_hash_table_insert(cache->old_properties, old_item->property, old_item);
     }
@@ -911,9 +946,11 @@ xfconf_cache_set(XfconfCache *cache,
 
         g_hash_table_insert(cache->pending_calls, old_item->cancellable, old_item);
 
-        if(item)
-            xfconf_cache_item_update(item, value);
-        else {
+        if(item) {
+            if (item->value != NULL) {
+                xfconf_cache_item_update(item, value);
+            }
+        } else {
             item = xfconf_cache_item_new(value, FALSE);
             g_tree_insert(cache->properties, g_strdup(property), item);
         }

common/xfconf-common-private.h
@@ -21,4 +21,11 @@
 #ifndef __XFCONF_COMMON_PRIVATE_H__
 #define __XFCONF_COMMON_PRIVATE_H__
 
+#include <glib.h>
+
+enum _XfconfError;
+
+gboolean _xfconf_error_from_dbus_error_name(const gchar *error_name,
+                                            enum _XfconfError *xfconf_error);
+
 #endif  /* __XFCONF_COMMON_PRIVATE_H__ */

common/xfconf-errors.c
@@ -26,6 +26,7 @@
 
 #include "xfconf/xfconf-errors.h"
 #include "xfconf-alias.h"
+#include "xfconf-common-private.h"
 
 /**
  * SECTION:xfconf-errors
@@ -110,6 +111,22 @@ xfconf_error_get_type(void)
 }
 
 
+gboolean
+_xfconf_error_from_dbus_error_name(const gchar *error_name,
+                                   XfconfError *xfconf_error)
+{
+    g_return_val_if_fail(error_name != NULL, FALSE);
+    g_return_val_if_fail(xfconf_error != NULL, FALSE);
+
+    for (gsize i = 0; i < G_N_ELEMENTS(xfconf_daemon_dbus_error_entries); ++i) {
+        if (g_strcmp0(error_name, xfconf_daemon_dbus_error_entries[i].dbus_error_name) == 0) {
+            *xfconf_error = xfconf_daemon_dbus_error_entries[i].error_code;
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
 
 #define __XFCONF_ERRORS_C__
 #include "xfconf-aliasdef.c"

xfconf/xfconf-errors.h
@@ -48,7 +48,7 @@ G_BEGIN_DECLS
  * An enumeration listing the different kinds of errors under the XFCONF_ERROR domain.
  *
  **/
-typedef enum
+typedef enum _XfconfError
 {
     XFCONF_ERROR_UNKNOWN = 0,
     XFCONF_ERROR_CHANNEL_NOT_FOUND,

commit e617c3a8b3a9c0badde6509e10ca70abc62083e0

cache: Fix overwritten error
Fixes: 6561c9fe940d4acb07c5eae63867c46683a02f2c
Related: !33

xfconf/xfconf-cache.c
@@ -791,7 +791,11 @@ xfconf_cache_lookup_locked(XfconfCache *cache,
                 g_free(error_name);
             }
 
-            g_propagate_error(error, tmp_error);
+            if (!item) {
+                g_propagate_error(error, tmp_error);
+            } else {
+                g_error_free(tmp_error);
+            }
         }
     }
 
@@ -878,7 +882,8 @@ xfconf_cache_set(XfconfCache *cache,
             if(G_LIKELY(g_dbus_error_is_remote_error (tmp_error)))
                 dbus_error_name = g_dbus_error_get_remote_error (tmp_error);
 
-            if(g_strcmp0(dbus_error_name, XFCONF_SERVICE_NAME_PREFIX ".Xfconf.Error.PropertyNotFound") != 0
+            if(dbus_error_name != NULL
+               && g_strcmp0(dbus_error_name, XFCONF_SERVICE_NAME_PREFIX ".Xfconf.Error.PropertyNotFound") != 0
                && g_strcmp0(dbus_error_name, XFCONF_SERVICE_NAME_PREFIX ".Xfconf.Error.ChannelNotFound") != 0)
             {
                 /* this is bad... */